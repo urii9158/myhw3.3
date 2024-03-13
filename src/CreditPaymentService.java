797a80102ad45752db796cd466409f1213df82a7
uri <katyacitti@gmail.com>
2024-03-13 19:42:04 +0200

    code component replacement

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
     public int calculate(int periodInYears, double interestRate, int amountOfCredit) {
         double monthlyInterestRate = interestRate / (12 * 100);
         int numberOfPayments = periodInYears * 12;
-        double monthlyPayment = (monthlyInterestRate * amountOfCredit) / (1 - Math.pow((1 + interestRate), -numberOfPayments));
+        double monthlyPayment = (monthlyInterestRate * amountOfCredit) / (1 - Math.pow((1 + monthlyInterestRate), -numberOfPayments));
         return (int) monthlyPayment;
     }
 }
